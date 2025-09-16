bf6b8e29e9bb0fc8b84d61d69f8a578497e1dbd3
Ashwini <bhawar.ashwini2892@gmail.com>
2025-09-16 18:20:53 +0530

    added Jenkinsfile

--- src/main/java/com/qa/opencart/factory/DriverFactory.java
+++ src/main/java/com/qa/opencart/factory/DriverFactory.java
@@ -112,7 +112,7 @@ public class DriverFactory {
 					ip = new FileInputStream("./src/test/resources/config/config.dev.properties");
 					break;
 				case "prod":
-					ip = new FileInputStream("./src/test/resources/config/config.properties");
+					ip = new FileInputStream("./src/test/resources/config/config.prod.properties");
 					break;
 				default:
 					log.error("Env value is invalid..please pass the right env value");
